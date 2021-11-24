be65f23607f647a5e8e4be0c013eb3e809e54440
Subroto <subroto_mohonto>
2021-11-24 10:38:11 +0600

    home footer added

---
+++ MascoAutomationApp/home/HeaderFooter.swift
@@ -0,0 +1,20 @@
+//
+//  HeaderFooter.swift
+//  MascoAutomationApp
+//
+//  Created by masco bazar on 24/11/21.
+//
+
+import UIKit
+
+class HeaderFooter: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
