dde4000327fda75d760a2a8acd64796bcdcf694d
Subroto <subroto_mohonto>
2021-12-05 13:49:54 +0600

    api calling

---
+++ MascoAutomationApp/Util/API.swift
@@ -0,0 +1,14 @@
+//
+//  API.swift
+//  MascoAutomationApp
+//
+//  Created by masco bazar on 5/12/21.
+//
+
+import Foundation
+
+
+var BASE_URL = "https://mis-api.mascoknit.com/api/"
+
+
+var LOGIN_URL = BASE_URL+"v1/LoginAccess/login"
--- MascoAutomationApp/ViewController.swift
+++ MascoAutomationApp/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var signInBtn: UIButton!
+    
+    var empCodeString = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -52,14 +55,27 @@ class ViewController: UIViewController {
 
             
                     
-        
+     
         if let text = empId.text, text.isEmpty {
             print("--Enter EmpId--")
         } else if let text1 = password.text, text1.isEmpty {
             print("--Enter password--")
         }else{
-            let controller = HomeViewController.initWithStoryboard()
-            self.present(controller, animated: true, completion: nil);
+            let _empId: String = empId.text!
+            let _password: String = password.text!
+            
+            let x = userLogin(userId: _empId, password: _password)
+            
+            print("--t--888888   \(x)-----")
+            
+            print("-------empCodeString--**********---: \(self.empCodeString)")
+            
+//
+//            if userLogin(userId: _empId, password: _password) != "" {
+//
+//            }
+           
+           // userLogin()
         }
         
     }
@@ -76,5 +92,217 @@ class ViewController: UIViewController {
         self.present(controller, animated: true, completion: nil);
     }
     
+    
+    func userLogin(userId: String, password: String)->String{
+        
+       
+        let url = URL(string: LOGIN_URL)
+        guard let requestUrl = url else { fatalError() }
+        
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "POST"
+        
+        // Set HTTP Request Header
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
 
+        let newTodoItem = ToDoRequestModel(empId: userId, password: password)
+        let jsonData = try? JSONEncoder().encode(newTodoItem)
+        
+        
+
+        request.httpBody = jsonData
+
+        print("jsonData jsonData  data:\n \(jsonData!)")
+        
+            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                
+                DispatchQueue.main.async {
+                
+                    
+                    if let error = error {
+                        print("Error took place \(error)")
+                        return
+                    }
+                    guard let data = data else {return}
+
+                    do{
+                        
+                        
+                        let todoItemModel = try JSONDecoder().decode(LoginResponse.self, from: data)
+                        print("Response data:\n \(todoItemModel)")
+                        print("todoItemModel Title: \(todoItemModel.mobile)")
+                        print("todoItemModel empCode: \(todoItemModel.empCode)")
+                        print("todoItemModel empId: \(todoItemModel.empId!)")
+                        print("todoItemModel token: \(todoItemModel.token)")
+                        print("todoItemModel refresh_token: \(todoItemModel.refresh_token)")
+                        print("todoItemModel error: \(todoItemModel.error)")
+                        
+                        self.empCodeString = todoItemModel.empCode
+                        
+                       
+                        
+                        for permission in todoItemModel._permissionList {
+                            
+                            print("todoItemModel -----: \(permission.moduleName)")
+                            print("_subMenuList -----: \(permission._subMenuList)")
+                            
+                            for subMenuList in permission._subMenuList {
+                                print("activityName -----: \(subMenuList.activityName)")
+                            }
+                          
+                        }
+                        
+
+                        print("-------empCodeString--++++++---: \(self.empCodeString)")
+                        
+                                            if !self.empCodeString.isEmpty{
+                                                let controller = HomeViewController.initWithStoryboard()
+                                                self.present(controller, animated: true, completion: nil);
+                                                print("----error: \("Success")----")
+                                            }else{
+                                               // print("----error: \(todoItemModel.error)----")
+                                            }
+                        
+                        
+                    }catch let jsonErr{
+                        print(jsonErr)
+                   }
+                   
+                }
+                
+                
+        }
+     
+        task.resume()
+        
+        return empCodeString
+    }
+    
+    func pageNavigation(){
+        
+        let controller = HomeViewController.initWithStoryboard()
+        self.present(controller, animated: true, completion: nil);
+    }
+
+}
+
+extension ViewController{
+    
+    struct ToDoRequestModel: Codable {
+        var empId: String
+        var password: String
+        
+        enum CodingKeys: String, CodingKey {
+                        case empId = "empId"
+                        case password = "password"
+            
+        }
+    }
+    
+    struct LoginResponse: Codable {
+        var empCode: String = ""
+        var mobile: String = ""
+        var empId: Int?
+        var token: String  = ""
+        var refresh_token: String  = ""
+        var error: String = ""
+        var _permissionList : [PermissionList]
+
+        enum CodingKeys: String, CodingKey {
+            case empCode = "empCode"
+            case mobile = "mobile"
+            case empId = "empId"
+            case token = "token"
+            case refresh_token = "refresh_token"
+            case error = "error"
+            case _permissionList
+        }
+        
+         init(from decoder: Decoder) throws {
+
+                let container = try decoder.container(keyedBy: CodingKeys.self)
+                self.empCode = try container.decodeIfPresent(String.self, forKey: .empCode) ?? ""
+                self.mobile = try container.decodeIfPresent(String.self, forKey: .mobile) ?? ""
+                self.empId = try container.decodeIfPresent(Int.self, forKey: .empId) ?? 0
+                self.token = try container.decodeIfPresent(String.self, forKey: .token) ?? ""
+                self.refresh_token = try container.decodeIfPresent(String.self, forKey: .refresh_token) ?? ""
+                self.error = try container.decodeIfPresent(String.self, forKey: .error) ?? ""
+            
+               self._permissionList = try container.decodeIfPresent([PermissionList].self, forKey: ._permissionList) ?? []
+            }
+
+            func encode(to encoder: Encoder) throws {
+
+                var container = encoder.container(keyedBy: CodingKeys.self)
+                try container.encode(empCode, forKey: .empCode)
+                try container.encode(mobile, forKey: .mobile)
+                try container.encode(empId, forKey: .empId)
+                try container.encode(token, forKey: .token)
+                try container.encode(refresh_token, forKey: .refresh_token)
+                try container.encode(error, forKey: .error)
+                try container.encode(_permissionList, forKey: ._permissionList)
+            }
+
+    }
+    //
+    struct PermissionList: Codable {
+        var moduleName: String = ""
+        var _subMenuList: [SubMenuList]
+        
+        enum CodingKeys: String, CodingKey {
+            case moduleName = "moduleName"
+            case _subMenuList
+            
+        }
+        
+        init(from decoder: Decoder) throws {
+
+               let container = try decoder.container(keyedBy: CodingKeys.self)
+               self.moduleName = try container.decodeIfPresent(String.self, forKey: .moduleName) ?? ""
+              self._subMenuList = try container.decodeIfPresent([SubMenuList].self, forKey: ._subMenuList) ?? []
+           }
+
+           func encode(to encoder: Encoder) throws {
+
+               var container = encoder.container(keyedBy: CodingKeys.self)
+               try container.encode(moduleName, forKey: .moduleName)
+              try container.encode(_subMenuList, forKey: ._subMenuList)
+           }
+    }
+
+    struct SubMenuList: Codable {
+        var activityName: String = ""
+        var manuId: Int = 0
+        var manuStepId: Int = 0
+        var parantManuId: Int = 0
+        var _specialPermission: String = ""
+        
+        enum CodingKeys: String, CodingKey {
+            case activityName = "activityName"
+            case manuId = "manuId"
+            case manuStepId = "manuStepId"
+            case parantManuId = "parantManuId"
+            case _specialPermission = "_specialPermission"
+            
+        }
+        
+        
+        init(from decoder: Decoder) throws {
+
+               let container = try decoder.container(keyedBy: CodingKeys.self)
+               self.activityName = try container.decodeIfPresent(String.self, forKey: .activityName) ?? ""
+               self.manuId = try container.decodeIfPresent(Int.self, forKey: .manuId) ?? 0
+               self.manuStepId = try container.decodeIfPresent(Int.self, forKey: .manuStepId) ?? 0
+               self.parantManuId = try container.decodeIfPresent(Int.self, forKey: .parantManuId) ?? 0
+               self._specialPermission = try container.decodeIfPresent(String.self, forKey: ._specialPermission) ?? ""
+           }
+
+           func encode(to encoder: Encoder) throws {
+
+               var container = encoder.container(keyedBy: CodingKeys.self)
+               try container.encode(activityName, forKey: .activityName)
+           }
+        
+    }
 }
